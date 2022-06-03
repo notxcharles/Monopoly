56129af5018a549fa738404da13f4a5076e73ca3
Charles <35784256+notxcharles@users.noreply.github.com>
2022-06-03 14:54:52 +0100

    Chance of ending a turn at x space
    Jail, community chest and chance cards not currently working.
    Modifies the css for the monopoly board to show the number of turns landed on at each square.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from unittest import defaultTestLoader
 from board_info.spaces import spaces
 from board_info.properties import properties
 import config
 import random
+import matplotlib
+import cssutils
 
 player_stats = []
 
@@ -46,7 +47,7 @@ def playerMove(player, diceRoll, jail=False):
     # Board has 40 positions 0-39, if player rolls eg 6 whilst in position 39, their new positon = 45, should be 5
     if currentPosition >= 40:
         currentPosition -= 40
-    print("playerMove returns ", currentPosition)
+    print("playerMove returns, newPosition:", currentPosition)
     logSpaceLandedOn(currentPosition)
     return currentPosition
 
@@ -61,16 +62,59 @@ def playerAction(player):
     return player
 
 
+def displayHTML(spaces):
+    # Get the stats of what percentage of turns resulted on landing on x space
+
+    # Normalise data (0-1)
+    list = []
+    for space in spaces:
+        list.append(space["landed_on"])
+
+    xmin = min(list)
+    xmax = max(list)
+    print(xmin)
+    print(xmax)
+    for i, x in enumerate(list):
+        list[i] = (x - xmin) / (xmax - xmin)
+
+    # Find corresponding cmap color for normalised value
+    cmap = matplotlib.cm.get_cmap("OrRd")
+    print("normalized data: ", list)
+    for id, space in enumerate(spaces):
+        rgba = cmap(list[id])
+        hexcolor = matplotlib.colors.rgb2hex(rgba)
+        space["landed_on_normalised"] = list[id]
+        space["hexcolor"] = hexcolor
+
+    # Change CSS
+    parser = cssutils.parseFile("stats.css")
+    for rule in parser.cssRules:
+        for space in spaces:
+            id = "#position" + str(space["position"])
+
+            if "." in id:
+                id = id.replace(".", "")
+            try:
+                if rule.selectorText == id:
+                    rule.style.backgroundColor = space["hexcolor"]
+            except AttributeError as e:
+                pass
+
+    with open("style_new.css", "wb") as f:
+        f.write(parser.cssText)
+
+
 def playGame():
     initialiseGame(config)
     # print(player_stats)
     for turn in range(config.turnsPerPlayer):
         for x in range(config.numberOfPlayers):
-            a = playerAction(player_stats[x])
-            print(player_stats[x])
+            print(playerAction(player_stats[x]))
             # print(x, " moved, turn ", turn, " pos ", pos)
     print("\n\n Game Complete: ", spaces)
     print("\n Players Stats: ", player_stats)
+
+    displayHTML(spaces)
 
 
 playGame()
