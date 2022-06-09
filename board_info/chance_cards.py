56c56eb2ad28028716b0aaabe62120abdf8d84dc
Charles <35784256+notxcharles@users.noreply.github.com>
2022-06-09 16:35:52 +0100

    Chance Cards and Community Chests

---
+++ board_info/chance_cards.py
@@ -0,0 +1,25 @@
+chance_cards = [
+    {"name": "Advance to Go (Collect £200)"},
+    {"name": "Advance to Trafalgar Square. If you pass Go, collect £200"},
+    {"name": "Advance to Pall Mall. If you pass Go, collect £200"},
+    {
+        "name": "Advance to Nearest Station"  # If unowned, you may buy it from the bank. If owned, pay the owner twice the rental to which they are entitled.
+    },
+    {
+        "name": "Advance to Nearest Station"  # If unowned, you may buy it from the bank. If owned, pay the owner twice the rental to which they are entitled.
+    },
+    {
+        "name": "Advance to Utility"  # If unowned you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown
+    },
+    {"name": "Bank pays you dividend of £50"},
+    {"name": "Get Out of Jail Free"},
+    {"name": "Go Back 3 Spaces"},
+    {"name": "Go to Jail. Go directly to Jail, do not pass Go, do not collect £200"},
+    # {
+    #     "name": "Make general repairs on all your property. For each house pay £25. For each hotel pay £100"
+    # },
+    {"name": "Speeding fine £15"},
+    {"name": "Take a trip to Kings Cross Station. If you pass Go, collect £200"},
+    {"name": "You have been elected Chairman of the Board. Pay each player £50"},
+    {"name": "Your building loan matures. Collect £150"},
+]
---
+++ board_info/community_chests.py
@@ -0,0 +1,20 @@
+community_chests = [
+    {"name": "Advance to Go (Collect £200)"},
+    {"name": "Bank error in your favour. Collect £200"},
+    {"name": "Doctor's fee. Pay £50"},
+    {"name": "From sale of stock you get £50"},
+    {"name": "Get out of jail free"},
+    {"name": "Go to Jail. Go directly to jail, do not pass Go, do not collect £200"},
+    {"name": "Holiday fund matures. Recieve £100"},
+    {"name": "Income tax refund. Collect £20"},
+    {"name": "It is your birthday. Collect £10 from every player"},
+    {"name": "Life insurance matures. Collect £20"},
+    {"name": "Pay hospital fees of £100"},
+    {"name": "Pay school fees of £50"},
+    {"name": "Recieve £25 consultancy fee"},
+    # {
+    #     "name": "You are assessed for street repairs. £40 per house. £115 per hotel"
+    # },
+    {"name": "You have won second prize in a beauty contest. Collect £10"},
+    {"name": "You inherit 100"},
+]
